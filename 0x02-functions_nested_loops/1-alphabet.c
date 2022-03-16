bb055ccfb580c3f90918efb79f83a25ec895627f
Anu <anuoluwaakinsola1@gmail.com>
2022-03-15 20:19:03 -0700

    Task-01

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * print_alphabet - check the code for Holberton School students.
- * Description: Print the alphabet with _putchar
+ * Description: Print the alphabeth with _putchar
  * Return: Always 0.
  */
-void print_alphabet(void)
+int main(void)
 {
 	char c;
 
